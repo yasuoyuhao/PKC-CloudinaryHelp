c6361dc10a9b214ec89b7f8f2ba5c854df15b101
yasuoyuhao <yasuoyuhao@gmail.com>
2019-04-26 13:50:05 +0800

    init

---
+++ Helper/CloudinaryHelper.cs
@@ -0,0 +1,22 @@
+﻿using CloudinaryDotNet;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudinaryHelp.Helper
+{
+    public class CloudinaryHelper
+    {
+        private readonly Account account;
+        public CloudinaryHelper(string CloudName, string ApiKey, string ApiSecret)
+        {
+            account = new Account(CloudName, ApiKey, ApiSecret);
+        }
+
+        public Cloudinary GetCloudinaryClient()
+        {
+            return new Cloudinary(account);
+        }
+    }
+}
---
+++ Services/CloudinaryServices.cs
@@ -0,0 +1,64 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using CloudinaryHelp.Helper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CloudinaryHelp.Services
+{
+    public class CloudinaryServices
+    {
+        private readonly CloudinaryHelper helper;
+        private readonly IConfiguration configuration;
+
+        public CloudinaryServices(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            string CloudName = this.configuration["CloudinarySetting:CloudName"];
+            string ApiKey = this.configuration["CloudinarySetting:ApiKey"];
+            string ApiSecret = this.configuration["CloudinarySetting:ApiSecret"];
+
+            helper = new CloudinaryHelper(CloudName, ApiKey, ApiSecret);
+        }
+
+        public ImageUploadResult UpdateImage(string fileName, Stream stream, List<string> tags = null)
+        {
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(fileName, stream),
+            };
+
+            var tagsString = "";
+
+            if (tags != null)
+            {
+                foreach (var tag in tags)
+                {
+                    if (tags.FirstOrDefault() == tag)
+                    {
+                        tagsString += $"{tag}";
+                    }
+                    else
+                    {
+
+                        tagsString += $", {tag}";
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(tagsString))
+            {
+                uploadParams.Tags = tagsString;
+            }
+
+            Cloudinary client = helper.GetCloudinaryClient();
+            ImageUploadResult uploadResult = client.Upload(uploadParams);
+
+            return uploadResult;
+        }
+    }
+}
